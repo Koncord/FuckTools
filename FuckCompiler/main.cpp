644bd944d17d2c456ca72e2a745b3a806e55cf3f
Koncord <zsv@koncord.xyz>
2021-08-24 16:26:00 +0800

    FuckCompiler: print function names and opt level

--- FuckCompiler/main.cpp
+++ FuckCompiler/main.cpp
@@ -9,6 +9,28 @@
 #include "fucktranslator.hpp"
 #include "fuckoptimizer.hpp"
 
+std::string PrintFunc(Instruction::VMOpcode op) {
+    switch (op) {
+#define RET_OP(x) case Instruction::VMOpcode::x: return #x;
+        RET_OP(inc)
+        RET_OP(sub)
+        RET_OP(incWin)
+        RET_OP(decWin)
+        RET_OP(outChar)
+        RET_OP(inChar)
+        RET_OP(loopBegin)
+        RET_OP(loopEnd)
+        RET_OP(set)
+        RET_OP(copy)
+        RET_OP(mul)
+        RET_OP(scan)
+        RET_OP(mov)
+#undef  RET_OP
+        default:
+            return "invalid";
+    }
+}
+
 int main(int argc, char **argv) {
     auto _error = [](const char *msg) {
         std::cerr << "fuckc: " << msg << "\nexecution interrupted." << std::endl;
@@ -34,6 +56,8 @@ int main(int argc, char **argv) {
             optLevel = FuckOptimizer::OptLevel::motherfucker;
     }
 
+    std::cout << "Opt level: " << (argc >= 4 ? argv[3] : "none") << std::endl;
+
     std::ifstream file(argv[1]);
 
     if (!file.is_open())
@@ -46,7 +70,7 @@ int main(int argc, char **argv) {
 
     std::cout << "-----------------[NOT OPTIMIZED]-----------------\n";
     for (const auto &c : code)
-        std::cout << "\"" << (char) c.fn << "\" arg: " << c.arg.full << std::endl;
+        std::cout << "\"" << PrintFunc(c.fn) << "\" arg: " << c.arg.full << std::endl;
 
     std::cout << "-------------[APPLIED OPTIMIZATIONS]-------------\n";
     FuckOptimizer optimizer(code); // compiler and optimizer in 1
@@ -55,7 +79,7 @@ int main(int argc, char **argv) {
     std::cout << "-------------------[OPTIMIZED]-------------------\n";
 
     for (const auto &c : code)
-        std::cout << "\"" << (char) c.fn << "\" arg: " << c.arg.full << std::endl;
+        std::cout << "\"" << PrintFunc(c.fn) << "\" arg: " << c.arg.full << std::endl;
 
     std::string outfile;
     if (argc < 3) {
